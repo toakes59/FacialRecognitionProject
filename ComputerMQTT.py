774b36239fa3a7925253728e81873a62a59be09e
Thomas Oakes <toakes01@manhattan.edu>
2021-11-10 20:07:18 -0500

    Added face and eye images and ComputerMQTT and PiMQTT

---
+++ ComputerMQTT.py
@@ -0,0 +1,62 @@
+import cv2
+import paho.mqtt.publish as publish
+
+# pip install cv2
+# sudo pip install paho-mqtt
+
+faceCascade = cv2.CascadeClassifier("Resources/haarcascade_frontalface_default.xml")
+
+cv2.namedWindow("Video")
+cap = cv2.VideoCapture(0)
+
+if cap.isOpened():  # try to get the first frame
+    success, frame = cap.read()
+else:
+    success = False
+cap.set(10, 100)
+
+xMiddle = 0
+yMiddle = 0
+xPrev = 0
+yPrev = 0
+xMove = 0
+yMove = 0
+xRatio = cap.get(3)  # Video capture width
+yRatio = cap.get(4)  # Video Capture height
+
+while success:
+    imgGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
+
+    # This is for debug
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    # finds center of face
+    xMiddle = (x + (w/2))
+    yMiddle = (y + (h/2))
+
+    # calculates distance between current center and previous center
+    xMove = xMiddle - xPrev
+    yMove = yMiddle - yPrev
+
+    # Determines how far x and y servo need to move
+    xMove = xMove / xRatio
+    yMove = yMove / yRatio
+
+    publish.single("CoreElectronics/x", xMove, hostname="test.mosquitto.org")
+    publish.single("CoreElectronics/y", yMove, hostname="test.mosquitto.org")
+
+    print(xMove)
+    print(yMove)
+
+    # debug code
+    #cv2.imshow("Video", frame)
+
+    success, frame = cap.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+
+cv2.destroyAllWindows()
---
+++ PiMQTT.py
@@ -0,0 +1,44 @@
+from gpiozero import Servo
+from time import sleep
+import paho.mqtt.client as mqtt
+
+# pip install cv2
+# sudo pip install paho-mqtt
+
+def moveX(move):
+    servoX.value = move
+
+def moveY(move):
+    servoY.value = move
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() - if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("CoreElectronics/test")
+    client.subscribe("CoreElectronics/topic")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+    if msg.topic == "CoreElectronics/x":
+        moveX(msg.payload)
+
+    if msg.topic == "CoreElectronics/y":
+        moveY(msg.payload)
+
+
+servoX = Servo(18)
+servoY = Servo(25)
+
+# Create an MQTT client and attach our routines to it.
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("test.mosquitto.org", 1883, 60)
+
+client.loop_forever()
